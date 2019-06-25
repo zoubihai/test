4dada2750c2b674b7482377b4d6899db4283daec
zoubihai <1208842419@qq.com>
2019-06-25 11:50:39 +0800

    fist commit

--- common/src/main/java/com/lingan/common/ARouterPath.java
+++ common/src/main/java/com/lingan/common/ARouterPath.java
@@ -1,6 +1,6 @@
 package com.lingan.common;
 
 public class ARouterPath {
-    public static final String GANK_MAIN_ACTIVITY = "/gank/1";
-    public static final String WAN_ANDROID_MAIN_ACTIVITY = "/wanandroid/2";
+    public static final String GANK_MAIN_ACTIVITY = "/gank/activity";
+    public static final String WAN_ANDROID_MAIN_ACTIVITY = "/wanandroid/activity";
 }
